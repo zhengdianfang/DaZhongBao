75a6ead33db8c419dc35c27542f5004c51e41a7c
zhengdianfang <dfgzheng@thoughtworks.com>
2017-07-29 20:39:30 +0800

    add login activity layout file

---
+++ app/src/main/java/com/zhengdianfang/dazhongbao/views/login/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.zhengdianfang.dazhongbao.views.login
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.zhengdianfang.dazhongbao.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+
+}
